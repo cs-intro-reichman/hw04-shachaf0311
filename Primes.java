43b68566d6144963b79c1840e97c991f41acb363
shachaf kuyumgiski <shachaf.kuyumgiski@post.runi.ac.il>
2025-11-19 14:43:51 +0200

    first try

--- Primes.java
+++ Primes.java
@@ -1,5 +1,37 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int max = Integer.parseInt(args[0]);
+        boolean [] isPrime = new boolean[max + 1];
+        for (int i = 2; i <= max; i++) 
+        {
+            isPrime[i] = true;
+        }
+        int p = 2;
+        while (p * p < max) 
+        {
+            if (isPrime[p]) 
+            {
+                for (int st = p;  st<= max; st++) 
+                {
+                   if (st % p == 0 && st != p) 
+                   {
+                       isPrime[st] = false;
+                   }
+                }
+            }
+            p++;
+        }
+        System.out.println("Prime numbers up to " + max + ":");
+        int count = 0;
+        for (int i = 2; i <= max; i++) 
+        {
+            if (isPrime[i]) 
+            {
+                System.out.println(i);
+                count++;
+            }
+        }
+        double percentage = (count * 100) / (max);
+        System.out.println("There are " + count + " primes between " + "2 and " + max + " (" + (int)percentage + "% are primes)");
     }
 }
